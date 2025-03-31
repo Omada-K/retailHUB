8089407da19fc027a8bbd397ae0f60ed6d557bf4
tBaronDar <thedarelis@gmail.com>
2025-03-31 19:00:57 +0300

    worked on main frame

---
+++ src/main/java/LoginFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class LoginFrame {
+  private JPanel loginPanel;
+  private JTextField inputUsername;
+  private JTextField inputPassword;
+  private JButton btnLogin;
+  private JButton cancelButton;
+}
---
+++ src/main/java/MainFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MainFrame {
+  private JButton logOutButton;
+  private JButton viewInvoicesButton;
+  private JButton viewUsersButton;
+  private JButton viewCustomersButton;
+  private JButton viewKatiAlloButton;
+  private JTabbedPane tabbedPane1;
+  private JCheckBox prediction1CheckBox;
+  private JCheckBox prediction2CheckBox;
+  private JCheckBox prediction3CheckBox;
+  private JButton generateButton;
+}
