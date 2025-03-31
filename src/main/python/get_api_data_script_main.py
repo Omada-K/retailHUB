194b04032364063278557e915b7cb39add5474e9
Stamalexx <stamelosalexios@gmail.com>
2025-03-31 10:14:02 +0300

    adds comments and description

--- src/main/python/get_api_data_script_main.py
+++ src/main/python/get_api_data_script_main.py
@@ -1,13 +1,23 @@
+'''
+1)Authentication & Token Retrieval – The script connects to Salesforce's OAuth2 authentication endpoint
+to obtain an access token using client credentials.
+2)Data Request & Retrieval – Using the access token, the script makes an authenticated request to a
+REST API endpoint to fetch CSV-formatted data.
+3)Data Processing & Storage – The retrieved CSV data is processed using Pandas, displayed for verification,
+and saved as a local file for further analysis.
+'''
+
 import requests
 import io
 import pandas as pd
-
+##API requirements see API documentation
 api_url =  "https://login.salesforce.com/services/oauth2/token"
 client_id = "3MVG9dAEux2v1sLvxtmY6Z_yz1e8.CJKTAx0varvs97mY_jMn_TaXhdOXnw13wSDg7t3PO0xSEIuMCpAnTksN"
 client_secret = "6BDB78C42DF09D218888040D8F643BD4E3E3F8E020E318F0EAB7D157D24A070B"
 username = "CAPSTONE@UOM.integration"
 password = "capstone@2025ul2MnLZyLm9yeJ4hHk5eOHUGh"
-##parameters
+
+###dictonary parameters
 header_dic = {
     "Content-Type":"application/x-www-form-urlencoded"
 }
@@ -19,29 +29,29 @@ parameters = {
     "password":password
 }
 
-#body
+## from requests library we are using .post method to get our data
 response = requests.post(url=api_url,params=parameters,headers=header_dic)
-response.raise_for_status() #ignore - check for success response
-data = response.json()
+response.raise_for_status() #ignore - checks for success response
+data = response.json() #saves data to json in the memory
 
-access_token = data['access_token']
-instance_url = data['instance_url']
+access_token = data['access_token'] #takes a specific data from the json data
+instance_url = data['instance_url'] #same
 
 ###second api
-second_api_URL = f"{instance_url}/services/apexrest/capstone/"
-##Bearer and access_token NEEDS A SPACE between !!@@
+second_api_URL = f"{instance_url}/services/apexrest/capstone/" #using url from the first API
+
+#same
 second_api_header = {
     "Authorization":f"Bearer {access_token}",
     "Content-Type":"Application/json"
 }
 
-second_response = requests.post(url=second_api_URL,headers=second_api_header)
-second_response.raise_for_status()
+second_response = requests.post(url=second_api_URL,headers=second_api_header) #post method from request
+second_response.raise_for_status() #ignore
 
-df = pd.read_csv(io.StringIO(second_response.text))
-print(df)
+df = pd.read_csv(io.StringIO(second_response.text)) # StringIO:creates an in-memory file-like object from that string - pandas reads the CSV
+print(df) #prints the data for validation
 
-
-df.to_csv("../data/raw_data_salesforce.csv") #works fine
+df.to_csv("../data/raw_data_salesforce.csv") #creates a csv file with our data
 
 
