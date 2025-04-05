89c40f7c07fe0025d544834cebe00a14981eed72
tBaronDar <thedarelis@gmail.com>
2025-04-05 23:05:13 +0300

    added more tasks, listener, modified forms

--- src/main/java/com/ui/MainFrame.java
+++ src/main/java/com/ui/MainFrame.java
@@ -11,12 +11,12 @@ public class MainFrame extends JFrame {
   private JButton viewUsersButton;
   private JButton viewCustomersButton;
   private JButton viewKatiAlloButton;
-  private JTabbedPane tabbedPane1;
   private JCheckBox prediction1CheckBox;
   private JCheckBox prediction2CheckBox;
   private JCheckBox prediction3CheckBox;
   private JButton generateButton;
   private JPanel mainFramePanel;
+  private JButton salvageDataFromOldButton;
 
   public MainFrame (TableFrame tableFrame) {
     setContentPane(mainFramePanel);// don't forget this, the window will be empty
@@ -31,6 +31,35 @@ public class MainFrame extends JFrame {
       public void actionPerformed (ActionEvent e) {
         dispose();
         tableFrame.setVisible(true);
+      }
+    });
+    //view users OnClick event listener
+    viewUsersButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        //TASK make click show the table.
+      }
+    });
+    //invoices OnClick event listener
+    viewInvoicesButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        //TASK make button show table
+      }
+    });
+    //logout OnClick event listener
+    logoutButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        //TASK make button open login frame and close current frame
+      }
+    });
+    //OnClick calls python cleaner
+    salvageDataFromOldButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        //TASK call the python script that calls the API
+        //Hint look in /service
       }
     });
   }
