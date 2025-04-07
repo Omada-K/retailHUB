06ff6c0a5c5e25926c1b6acabe2623b95dbc6ea8
Stamalexx <stamelosalexios@gmail.com>
2025-04-07 16:09:56 +0300

    making progress in the login auth function - i got some questions

--- src/main/java/com/ui/LoginFrame.java
+++ src/main/java/com/ui/LoginFrame.java
@@ -1,8 +1,11 @@
 package com.ui;
 
+import com.model.User;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class LoginFrame extends JFrame {
   private JPanel loginPanel;
@@ -10,6 +13,8 @@ public class LoginFrame extends JFrame {
   private JTextField inputPassword;
   private JButton btnLogin;
   private JButton cancelButton;
+  private ArrayList<User> usernameList; //NADOUME AUTO - alex
+  private ArrayList<User> passwordList;//NADOUME AUTO - alex
 
   public LoginFrame (MainFrame mainFrame) {
     setContentPane(loginPanel);// don't forget this, the window will be empty
@@ -23,11 +28,18 @@ public class LoginFrame extends JFrame {
       @Override
       public void actionPerformed (ActionEvent e) {
         //TASK work here to create auth logic
-        String txt = getInputUsername().getText();
-        System.out.println(txt);
+        String username = getInputUsername().getText();
+        String password = getInputPassword().getText();
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+        Auth creds = new Auth(username,password,usernameList,passwordList); //NADOUME AUTO - alex
 
         dispose();//this makes THIS frame not visible
-        mainFrame.setVisible(true);
+        if (creds.Checker())
+          mainFrame.setVisible(true);
+        else
+          System.out.println("Ti kaneis ekei atimouli?");
+        creds.authentication_message();
       }
     };
     //connect listener to the button
@@ -42,6 +54,9 @@ public class LoginFrame extends JFrame {
   public JTextField getInputUsername () {
     return inputUsername;
 
+  }
+  public JTextField getInputPassword () {
+    return inputPassword;
   }
 
 }
