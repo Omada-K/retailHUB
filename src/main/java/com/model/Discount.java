70a1dff2abf6a56a538240bf3d98637bf3aeb67c
ParaskeviKat <paraskevikatidoy@gmail.com>
2025-04-23 20:18:27 +0300

    Update Discount.java

--- src/main/java/com/model/Discount.java
+++ src/main/java/com/model/Discount.java
@@ -1,1 +1,19 @@
+package com.model;
 
+public class Discount {
+  private int orderId;
+  private double amount;
+
+  //Constructor
+  public Discount (int orderId, double amount) {
+    this.orderId = orderId;
+    this.amount = amount;
+  }
+
+  //Getters & Setters
+  public int getOrderId () { return orderId; }
+  public void setOrderId (int orderId) { this.orderId = orderId; }
+
+  public double getAmount () { return amount; }
+  void setAmount (double amount) { this.amount = amount; }
+}
