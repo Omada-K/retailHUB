ba0a198ca6a01682f7a0ae5eb138eb43be1ddb6e
ParaskeviKat <paraskevikatidoy@gmail.com>
2025-04-23 20:20:42 +0300

    Update Customer.java

--- src/main/java/com/model/Customer.java
+++ src/main/java/com/model/Customer.java
@@ -20,26 +20,20 @@ private int CustomerId;
 
   //Getters & Setters
   public int getCustomerId () { return CustomerId; }
-
   public void setCustomerId (int CustomerId) { this.CustomerId = CustomerId; }
 
   public String getName () { return name; }
-
   public void setName (String name) { this.name = name; }
 
   public String getAddress () { return address; }
-
   public void setAddress (String address) { this.address = address; }
 
   public String getPhone () { return phone; }
-
   public void setPhone (String phone) { this.phone = phone; }
 
   public String getEmail () { return email; }
-
   public void setEmail (String email) { this.email = email; }
 
   public int getUserId () { return UserId; }
-
   public void setUserId (int UserId) { this.UserId = UserId; }
 }
