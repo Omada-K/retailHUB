1369002ad5d555ccb8d9fd8cc49423f98b51e4b5
Paraskevi <paraskevikatidoy@gmail.com>
2025-04-23 19:25:42 +0300

    test

---
+++ src/main/java/com/model/Discount.java
@@ -0,0 +1,21 @@
+package com.model;
+
+public class Discount {
+  private int orderId;
+  private double amount;
+
+  //Constructor
+  public Discount (int orderId, double amount) {
+    this.orderId = orderId;
+    this.amount = amount;
+  }
+
+  //getters setters
+  public int getOrderId () { return orderId; }
+
+  public void setOrderId (int orderId) { this.orderId = orderId; }
+
+  public double getAmount () { return amount; }
+
+  void setAmount (double amount) { this.amount = amount; }
+}
