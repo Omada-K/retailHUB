34b044df150c9415f0d51b0a54d45cfd3f220939
ParaskeviKat <paraskevikatidoy@gmail.com>
2025-04-24 23:31:46 +0300

    Update Product.java

--- src/main/java/com/model/Product.java
+++ src/main/java/com/model/Product.java
@@ -2,22 +2,29 @@ package com.model;
 
 public class Product {
   private int productId;
+  private String productCategory;
   private String productName;
   private String productDescription;
   private double productPrice;
 
   //Constructor
-  public Product (int productId, String productName, String productDescription, double productPrice) {
+  public Product (int productId, String productCategory, String productName, String productDescription, double productPrice) {
     this.productId = productId;
+    this.productCategory = productCategory;
     this.productName = productName;
     this.productDescription = productDescription;
     this.productPrice = productPrice;
   }
 
   //Getters & Setters
+
   public int getProductId () { return productId; }
 
   public void setProductId (int productId) { this.productId = productId; }
+
+  public String getProductCategory () { return productCategory; }
+
+  public void setProductCategory (String productCategory) { this.productCategory = productCategory; }
 
   public String getProductName () { return productName; }
 
@@ -30,4 +37,5 @@ public class Product {
   public double getProductPrice () { return productPrice; }
 
   public void setProductPrice (double productPrice) { this.productPrice = productPrice; }
+
 }
