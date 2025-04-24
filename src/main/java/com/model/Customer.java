517bbb85edbd910053dc3e9c715e5df9c95ff49a
ParaskeviKat <paraskevikatidoy@gmail.com>
2025-04-24 23:27:52 +0300

    Update Customer.java

--- src/main/java/com/model/Customer.java
+++ src/main/java/com/model/Customer.java
@@ -1,26 +1,24 @@
 package com.model;
 
 public class Customer {
-private int customerId;
+  private int CustomerId;
   private String name;
   private String address;
   private String phone;
   private String email;
-  private int userId;
 
   //Constructor
-  public Customer (int customerId, String name, String address, String phone, String email, int userId) {
-    this.customerId = customerId;
+  public Customer (int CustomerId, String name, String address, String phone, String email) {
+    this.CustomerId = CustomerId;
     this.name = name;
     this.address = address;
     this.phone = phone;
     this.email = email;
-    this.userId = userId;
   }
 
   //Getters & Setters
-  public int getCustomerId () { return customerId; }
-  public void setCustomerId (int customerId) { this.customerId = customerId; }
+  public int getCustomerId () { return CustomerId; }
+  public void setCustomerId (int CustomerId) { this.CustomerId = CustomerId; }
 
   public String getName () { return name; }
   public void setName (String name) { this.name = name; }
@@ -33,7 +31,4 @@ private int customerId;
 
   public String getEmail () { return email; }
   public void setEmail (String email) { this.email = email; }
-
-  public int getUserId () { return userId; }
-  public void setUserId (int userId) { this.userId = userId; }
 }
