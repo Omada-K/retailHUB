49ed7629a86480a4b6e545d179a653a7e36d82bf
tBaronDar <thedarelis@gmail.com>
2025-04-29 22:08:00 +0300

    added comments

--- src/main/java/com/service/DataGenerator.java
+++ src/main/java/com/service/DataGenerator.java
@@ -2,6 +2,8 @@ package com.service;
 
 public class DataGenerator {
   //create dummy data
-  // create static methods for each model that create dummy data
+  // create  methods for each model that creates dummy data
   // and put it in the db using the DAO
+  //tip: in the end create a static 'mega' method that calls all the
+  //smaller methods -see createAllTables() by chris
 }
